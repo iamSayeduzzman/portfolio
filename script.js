3424e0bed0d82bac18625440d1c5c88459131dd1
Sayeduzzman Emon <156037308+iamSayeduzzman@users.noreply.github.com>
2026-08-20 03:24:54 +0600

    Remove 'GitHub Explorer' from projects
    Removed 'GitHub Explorer' project from the projects list.

--- script.js
+++ script.js
@@ -33,14 +33,6 @@ const experienceData = [
 
 const projects = [
   {
-    title: "GitHub Explorer",
-    category: "web-dev",
-    icon: "🙋🏻‍♂️",
-    description: "Production-ready dashboard UI built with Next.js 16, Shadcn UI, TypeScript and Tailwind CSS.",
-    tags: ["React", "Redux Toolkit", "RTK Query", "React Query","Node.js", "SASS"],
-    github: "https://github.com/iamSayeduzzman/github-user-explorer",
-  },
-  {
     title: "Shadcn Dashboard Starter",
     category: "web-dev",
     icon: "🚀",
